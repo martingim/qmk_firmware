d54d8b7c4adbdcf92472d48039f8cbf499ff80bc
martingim <martingimse@gmail.com>
2024-02-07 08:36:51 +0100

    added gui key to two layers

--- keyboards/cantor/keymaps/martingim/keymap.c
+++ keyboards/cantor/keymaps/martingim/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	  [1] = LAYOUT_split_3x6_3( KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,              KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSPC, 
                               KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,                    KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL, 
                               KC_LSFT, KC_F11, KC_F12, LCA(KC_LEFT), LCA(KC_RGHT), KC_NO,       KC_PPLS, KC_PAST, KC_COMM, KC_DOT, KC_PSLS, KC_PMNS, 
-                                                    TO(0), KC_LSFT, TO(0),     LT(2,KC_ENT), LT(3,KC_SPC), KC_DEL),
+                                                    TO(0), KC_LSFT, KC_LGUI,     LT(2,KC_ENT), LT(3,KC_SPC), KC_DEL),
 	  
     [2] = LAYOUT_split_3x6_3( KC_NO, KC_NO, KC_EXLM, KC_DQUO, KC_DLR, KC_PERC,                      KC_BSLS, KC_TILD, KC_NO, KC_NO, KC_NO, KC_BSPC, 
                               KC_NO, KC_NO, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS,                     KC_COMM, KC_RPRN, KC_RBRC, KC_RCBR, KC_EQL, KC_NO, 
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = LAYOUT_split_3x6_3(LALT(KC_TAB), KC_NO, KC_NO, KC_UP, KC_NO, KC_NO,                                   KC_NO, KC_NO, KC_VOLU, KC_VOLD, KC_MUTE, KC_BSPC, 
                                   KC_NO, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,                            KC_HOME, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_END, 
                                   KC_NO, LCTL(KC_Y), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_Z),            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-                                                    TO(0), KC_LSFT, KC_NO,                               KC_ENT, KC_SPC, KC_NO),
+                                                    TO(0), KC_LSFT, KC_LGUI,                               KC_ENT, KC_SPC, KC_NO),
 	  
     [4] = LAYOUT_split_3x6_3( KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, 
                               KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                    KC_NO, KC_NO, TO(0), TO(0), KC_UP, TO(0), 
