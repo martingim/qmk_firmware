f2895018afc85e10fcc0d64a5c2fbbdfddf09f2f
martingim <martingimse@gmail.com>
2024-04-25 10:07:57 +0200

    changed layout for special characters and layer 4 and 6

--- keyboards/cantor/keymaps/martingim/keymap.c
+++ keyboards/cantor/keymaps/martingim/keymap.c
@@ -51,31 +51,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                   KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
                               KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TO(4), 
                                               TD(GUI_LAYR), KC_LSFT, KC_LALT,   LT(2,KC_ENT), LT(3,KC_SPC), LT(1,KC_DEL)),
-
-	  [1] = LAYOUT_split_3x6_3( KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,              KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSPC, 
+    //number layer
+	[1] = LAYOUT_split_3x6_3( KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,              KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSPC, 
                               KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,                    KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL, 
                               KC_LSFT, KC_F11, KC_F12, LCA(KC_LEFT), LCA(KC_RGHT), KC_NO,       KC_PPLS, KC_PAST, KC_COMM, KC_DOT, KC_PSLS, KC_PMNS, 
                                                     TO(0), KC_LSFT, KC_LGUI,     LT(2,KC_ENT), LT(3,KC_SPC), KC_DEL),
-	  
-    [2] = LAYOUT_split_3x6_3( KC_NO, KC_NO, KC_EXLM, KC_DQUO, KC_DLR, KC_PERC,                      KC_BSLS, KC_TILD, KC_NO, KC_NO, KC_NO, KC_BSPC, 
+	//special keys
+    [2] = LAYOUT_split_3x6_3( KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                    KC_BSLS, KC_AMPR, KC_NO, KC_NO, KC_NO, KC_BSPC, 
                               KC_NO, KC_NO, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS,                     KC_COMM, KC_RPRN, KC_RBRC, KC_RCBR, KC_EQL, KC_NO, 
                               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UNDS,                           KC_NO, KC_NO, KC_COMM, KC_DOT, KC_SLSH, KC_NO, 
                                                      TO(0), KC_NO, KC_TRNS,               KC_NO, KC_NO, KC_NO),
-	  
+	//movement
     [3] = LAYOUT_split_3x6_3(LALT(KC_TAB), KC_NO, KC_NO, KC_UP, KC_NO, KC_NO,                                   KC_NO, KC_NO, KC_VOLU, KC_VOLD, KC_MUTE, KC_BSPC, 
                                   KC_NO, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,                            KC_HOME, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_END, 
                                   KC_NO, LCTL(KC_Y), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_Z),            KC_NO, KC_NO, KC_PGUP, KC_PGDN, KC_NO, KC_NO, 
                                                     TO(0), KC_LSFT, KC_LGUI,                               KC_ENT, KC_SPC, KC_NO),
-	  
+	//game  
     [4] = LAYOUT_split_3x6_3( KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, 
-                              KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                    KC_NO, KC_NO, TO(0), TO(0), KC_UP, TO(0), 
-                              KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                   KC_NO, KC_NO, TO(0), KC_LEFT, KC_DOWN, KC_RGHT, 
-                                                  TO(0), KC_LSFT, KC_SPC,         KC_ENT, KC_SPC, TO(0)),
+                              KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                    KC_NO, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, TO(0), 
+                              KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                   KC_NO, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, TO(0),
+                                                  KC_LALT, KC_LSFT, LT(6,KC_ENT),         LT(2,KC_ENT), LT(3,KC_SPC), LT(1,KC_DEL)),
 
     [5] = LAYOUT_split_3x6_3( KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                    KC_ACL0, KC_NO, KC_WH_U, KC_WH_D, KC_NO, KC_BSPC, 
                               KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                    KC_ACL1, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, TO(0), 
                               KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                   KC_ACL2, KC_NO, TO(0), KC_LEFT, KC_DOWN, KC_RGHT, 
-                                                  TO(0), KC_LSFT, KC_SPC,         KC_ENT, KC_BTN1, KC_BTN2)
+                                                  TO(0), KC_LSFT, KC_SPC,         KC_ENT, KC_BTN1, KC_BTN2),
+                                
+    //number left hand
+	[6] = LAYOUT_split_3x6_3( KC_ESC, KC_F1, KC_F2, KC_1, KC_2, KC_3,              KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSPC, 
+                              KC_NO, KC_1, KC_2, KC_4, KC_5, KC_6,                    KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL, 
+                              KC_LSFT, KC_F11, KC_0, KC_7, KC_8, KC_9,       KC_PPLS, KC_PAST, KC_COMM, KC_DOT, KC_PSLS, KC_PMNS, 
+                                                    TO(0), KC_LSFT, KC_LGUI,     LT(2,KC_ENT), LT(3,KC_SPC), KC_DEL)
 };
 
 
