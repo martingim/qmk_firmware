2d2cf0ae36d0349f809d9b0ddf501e6dfd2c683b
martingim <martingimse@gmail.com>
2024-09-20 08:03:25 +0200

    added number layer

--- keyboards/geigeigeist/klor/keymaps/martin/keymap.c
+++ keyboards/geigeigeist/klor/keymaps/martin/keymap.c
@@ -47,17 +47,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                         └───┤Alt│   │Ent├───┘
       *                             └───┘   └───┘
       */
+    //base layer
     [0] = LAYOUT(
                     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P, 
-        KC_TAB,     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT, 
-        KC_DEL,     KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_MUTE,        KC_MPLY,    KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-                                        KC_LCTL, KC_TRNS, KC_SPC, KC_LALT,        KC_LGUI, KC_ENT, KC_TRNS, KC_BSPC),
+        KC_ESC,     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT, 
+    LCTL_T(KC_TAB), KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_MUTE,        KC_MPLY,    KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_BSPC, 
+                                    KC_LGUI, KC_NO, KC_LSFT, KC_LALT,        LT(2,KC_ENT), LT(3,KC_SPC), LT(1,KC_DEL), KC_BSPC),
+    //number layer
     [1] = LAYOUT(
-                    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P, 
-        KC_TAB,     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT, 
-        KC_DEL,     KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_MUTE,        KC_MPLY,    KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-                                        KC_LCTL, KC_TRNS, KC_SPC, KC_LALT,        KC_LGUI, KC_ENT, KC_TRNS, KC_BSPC),
-    
+                    KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                              KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, 
+        KC_ESC,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                               KC_6,   KC_7,   KC_8,   KC_9,   KC_0, KC_QUOT, 
+    LCTL_T(KC_TAB), KC_F11, KC_F12, LCA(KC_LEFT), LCA(KC_RGHT),KC_B,KC_MUTE, KC_MPLY,    KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_BSPC, 
+                                    KC_LGUI, KC_NO, KC_LSFT, KC_LALT,        LT(2,KC_ENT), LT(3,KC_SPC), LT(1,KC_DEL), KC_BSPC),
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
