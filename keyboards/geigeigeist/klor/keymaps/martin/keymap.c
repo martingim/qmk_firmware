c6d308af5cd2c69becd6abf1d3a58eb7c07198e3
martingim <martingimse@gmail.com>
2024-09-20 19:35:02 +0200

    more layers

--- keyboards/geigeigeist/klor/keymaps/martin/keymap.c
+++ keyboards/geigeigeist/klor/keymaps/martin/keymap.c
@@ -49,16 +49,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     //base layer
     [0] = LAYOUT(
-                    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P, 
-        KC_ESC,     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT, 
-    LCTL_T(KC_TAB), KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_MUTE,        KC_MPLY,    KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_BSPC, 
-                                    KC_LGUI, KC_NO, KC_LSFT, KC_LALT,        LT(2,KC_ENT), LT(3,KC_SPC), LT(1,KC_DEL), KC_BSPC),
+                    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P, 
+        KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT, 
+    LCTL_T(KC_TAB), KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,   KC_MUTE,                KC_MPLY,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_BSPC, 
+                                            KC_LGUI,    KC_NO,      KC_LSFT,    KC_LALT,        LT(2,KC_ENT),   LT(3,KC_SPC),LT(1,KC_DEL),KC_BSPC),
     //number layer
     [1] = LAYOUT(
-                    KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                              KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, 
-        KC_ESC,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                               KC_6,   KC_7,   KC_8,   KC_9,   KC_0, KC_QUOT, 
-    LCTL_T(KC_TAB), KC_F11, KC_F12, LCA(KC_LEFT), LCA(KC_RGHT),KC_B,KC_MUTE, KC_MPLY,    KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_BSPC, 
-                                    KC_LGUI, KC_NO, KC_LSFT, KC_LALT,        LT(2,KC_ENT), LT(3,KC_SPC), LT(1,KC_DEL), KC_BSPC),
+                    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                                      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10, 
+        KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_QUOT, 
+    LCTL_T(KC_TAB), KC_F11,     KC_F12,     LCA(KC_LEFT),LCA(KC_RGHT),KC_B, KC_MUTE,                KC_MPLY,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_BSPC, 
+                                            KC_LGUI,    KC_NO,      KC_LSFT,    KC_LALT,        LT(2,KC_ENT), LT(3,KC_SPC), LT(1,KC_DEL), KC_BSPC),
+
+    //special keys
+    [2] = LAYOUT(
+                    KC_EXLM, KC_AT,     KC_HASH,    KC_DLR,     KC_PERC,                                        KC_BSLS,    KC_AMPR,    KC_NO,      KC_NO,      KC_NO, 
+        KC_NO,      KC_NO,  KC_LCBR,    KC_LBRC,    KC_LPRN,    KC_MINS,                                        KC_COMM,    KC_RPRN,    KC_RBRC,    KC_RCBR,    KC_EQL,     KC_NO, 
+        KC_NO,      KC_NO,  KC_NO,      KC_NO,      KC_NO,      KC_UNDS,    KC_MUTE,                KC_MUTE,    KC_NO,      KC_NO,      KC_COMM,    KC_DOT,     KC_SLSH,    KC_NO, 
+                                                KC_NO,  TO(0),  KC_NO,  KC_TRNS,                        KC_NO,  KC_NO,      KC_NO,      KC_NO),
+	//movement
+    [3] = LAYOUT(
+                    KC_NO,      KC_NO,      KC_UP,      KC_NO,      KC_NO,                                      KC_NO,      KC_NO,      KC_VOLU,    KC_VOLD,    KC_MUTE, 
+        KC_NO,      KC_HOME,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_END,                                     KC_HOME,    KC_LEFT,    KC_UP,      KC_DOWN,    KC_RGHT,    KC_END, 
+        KC_NO,      LCTL(KC_Y), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_Z), KC_MUTE,            KC_MUTE,    KC_NO,      KC_NO,      KC_PGUP,    KC_PGDN,    KC_NO,      KC_NO, 
+                                            KC_NO,      TO(0),      KC_LSFT,    KC_LGUI,                KC_ENT, KC_SPC,     KC_NO,      KC_NO),
+	//game  
+    [4] = LAYOUT(
+                    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P, 
+        KC_TAB,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                       KC_H,       KC_LEFT,    KC_UP,      KC_DOWN,    KC_RGHT,    TO(0), 
+        KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_LALT, KC_MUTE,               KC_MUTE,    KC_N,       KC_M,       KC_UP,      KC_DOWN,    KC_RGHT,    TO(0),
+                                            KC_NO,      KC_SPC,     KC_LSFT,    LT(6,KC_ENT),     LT(2,KC_ENT), LT(3,KC_SPC), LT(1,KC_DEL), KC_NO),
+
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
