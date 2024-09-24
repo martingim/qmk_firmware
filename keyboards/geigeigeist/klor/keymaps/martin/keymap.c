1876b048e3cfe55df6c63382ab9bd2f24834ed43
martingim <martingimse@gmail.com>
2024-09-24 11:47:58 +0200

    small changes to number layer

--- keyboards/geigeigeist/klor/keymaps/martin/keymap.c
+++ keyboards/geigeigeist/klor/keymaps/martin/keymap.c
@@ -56,16 +56,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //number layer
     [1] = LAYOUT(
                     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                                      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10, 
-        KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_QUOT, 
-    LCTL_T(KC_TAB), KC_F11,     KC_F12,     LCA(KC_LEFT),LCA(KC_RGHT),KC_B, KC_MUTE,                KC_MPLY,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_BSPC, 
+        KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_EQL, 
+    LCTL_T(KC_TAB), KC_F11,     KC_F12,     LCA(KC_LEFT),LCA(KC_RGHT),KC_B, KC_MUTE,                KC_MPLY,    KC_PPLS,    KC_PAST,    KC_COMM,    KC_DOT,     KC_SLSH,    KC_QUOT, 
                                             KC_LGUI,    KC_LSFT,      KC_SPC,    KC_LALT,        LT(2,KC_ENT), LT(3,KC_SPC), LT(1,KC_DEL), LT(4, KC_BSPC)),
 
     //special keys
     [2] = LAYOUT(
-                    KC_EXLM, KC_AT,     KC_HASH,    KC_DLR,     KC_PERC,                                        KC_BSLS,    KC_AMPR,    KC_NO,      KC_NO,      KC_NO, 
-        KC_NO,      KC_NO,  KC_LCBR,    KC_LBRC,    KC_LPRN,    KC_MINS,                                        KC_COMM,    KC_RPRN,    KC_RBRC,    KC_RCBR,    KC_EQL,     KC_NO, 
-        KC_NO,      KC_NO,  KC_NO,      KC_NO,      KC_NO,      KC_UNDS,    KC_MUTE,                KC_MUTE,    KC_NO,      KC_NO,      KC_COMM,    KC_DOT,     KC_SLSH,    KC_NO, 
-                                        KC_LGUI,    KC_LSFT,    KC_SPC,     KC_TRNS,                    KC_NO,  KC_NO,      KC_NO,      KC_NO),
+                    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,                                        KC_BSLS,    KC_AMPR,    KC_NO,      KC_NO,      KC_NO, 
+        KC_NO,      KC_NO,      KC_LCBR,    KC_LBRC,    KC_LPRN,    KC_MINS,                                        KC_COMM,    KC_RPRN,    KC_RBRC,    KC_RCBR,    KC_EQL,     KC_NO, 
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_UNDS,    KC_MUTE,                KC_MUTE,    KC_NO,      KC_NO,      KC_COMM,    KC_DOT,     KC_SLSH,    KC_NO, 
+                                            KC_LGUI,    KC_LSFT,    KC_SPC,     KC_TRNS,                    KC_NO,  KC_NO,      KC_NO,      KC_NO),
 	//movement
     [3] = LAYOUT(
                     KC_NO,      KC_NO,      KC_UP,      KC_NO,      KC_NO,                                      KC_NO,      KC_NO,      KC_VOLU,    KC_VOLD,    KC_MUTE, 
